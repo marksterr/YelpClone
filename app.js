28a7e0d2e62ebb01f452e40f3d6238dae8c74289
Mark Dawes <mrkdawes8@gmail.com>
2022-02-27 18:47:20 -0500

    add review display

--- app.js
+++ app.js
@@ -87,7 +87,7 @@ app.post('/campgrounds', validateCampground, catchAsync(async (req, res, next) =
 
 // render campground show page
 app.get('/campgrounds/:id', catchAsync(async (req, res) => {
-    const campground = await Campground.findById(req.params.id);
+    const campground = await Campground.findById(req.params.id).populate('reviews');
     res.render('campgrounds/show', { campground });
 }));
 
