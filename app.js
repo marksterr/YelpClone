86975330495aeb4f93784eb1cbad0f69c90cd52e
Mark Dawes <mrkdawes8@gmail.com>
2022-02-26 22:18:28 -0500

    add validation middleware

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-// setup express, mongoose, ejs-mate, joi, async wrapper, exrpesserror, method-override, and import models
+// setup express, mongoose, ejs-mate, schemas, async wrapper, exrpesserror, method-override, and import models
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 const ejsMate = require('ejs-mate');
-const Joi = require('joi');
+const { campgroundSchema } = require('./schemas.js');
 const catchAsync = require('./utils/catchAsync');
 const ExpressError = require('./utils/ExpressError');
 const methodOverride = require('method-override');
@@ -37,6 +37,18 @@ app.use(express.urlencoded({ extended: true }));
 // configure express to handle all method requests
 app.use(methodOverride('_method'));
 
+// middleware function using joi for validation
+const validateCampground = (req, res, next) => {
+    // if validation error then pass to expresserror
+    const { error } = campgroundSchema.validate(req.body);
+    if(error){
+        const msg = error.details.map(el => el.message).join(',');
+        throw new ExpressError(msg, 400);
+    } else {
+        next();
+    }
+}
+
 // render home page
 app.get('/', (req, res) => {
     res.render('home');
@@ -54,25 +66,8 @@ app.get('/campgrounds/new', (req, res) => {
 });
 
 // save campground data when posted then redirect
-app.post('/campgrounds', catchAsync(async (req, res, next) => {
+app.post('/campgrounds', validateCampground, catchAsync(async (req, res, next) => {
     // if(!req.body.campground) throw new ExpressError('Invalid Campground Data', 400);
-    // use joi schema for validation
-    const campgroundSchema = Joi.object({
-        campground: Joi.object({
-            title: Joi.string().required(),
-            price: Joi.number().required().min(0),
-            image: Joi.string().required(),
-            location: Joi.string().required(),
-            description: Joi.string().required()
-        }).required()
-    });
-    // if validation error then pass to expresserror
-    const { error } = campgroundSchema.validate(req.body);
-    if(error){
-        const msg = error.details.map(el => el.message).join(',');
-        throw new ExpressError(msg, 400);
-    }
-    console.log(result);
     const campground = new Campground(req.body.campground);
     await campground.save();
     res.redirect(`/campgrounds/${campground._id}`);
@@ -91,7 +86,7 @@ app.get('/campgrounds/:id/edit', catchAsync(async (req, res) => {
 }));
 
 // update with posted campground data
-app.put('/campgrounds/:id', catchAsync(async (req, res) => {
+app.put('/campgrounds/:id', validateCampground, catchAsync(async (req, res) => {
     const { id } = req.params;
     const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
     res.redirect(`/campgrounds/${campground._id}`);
